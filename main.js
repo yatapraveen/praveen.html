73b2996a5b4af7a883e6e9fe65f6fd432f5b0a27
yatapraveen <139345435+yatapraveen@users.noreply.github.com>
2023-07-12 16:32:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// Mobile Navigation
+
+const mobileNav = document.querySelector(".mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const mobileMenuItems = document.querySelectorAll(".mobile-nav .menu-items li");
+
+menuIcon.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
+
+mobileMenuItems.forEach((i) => {
+  i.addEventListener("click", () => {
+    mobileNav.classList.remove("active");
+  });
+});
+
+// Desktop Navigation
+
+const options = {
+  threshold: 0.8,
+};
+
+const addActiveClass = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && entry.intersectionRatio >= 0.2) {
+      //   console.log(entry.target);
+      let currentActive = document.querySelector(".desktop-nav a.act…
